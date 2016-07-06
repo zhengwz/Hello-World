46d545ceeeddc4e09a0c1668a19f6adba38cc28b
zhengwz <zhengwz@users.noreply.github.com>
2016-07-06 19:31:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 <head><title>HelloWorld</title></head>
 <body>
 
-<?php>
+<?php
   $num1=1;
   $num2=2;
   echo $num1+num2;
   $num3=3;
   $num4=4;
   echo $num3+num4;
-<?>
+?>
+
+<from>
+  <input name="a",type="Text">
+  <input name="b",type="submit">
+</form>
 
 <body>
 </html>
